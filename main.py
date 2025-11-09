811d91b1935d957368090b85584bf822b03a2cfb
Afonso Barão <afonso.j.m.barao@gmail.com>
2025-11-09 11:27:29 +0000

    Reestruturação da class Trade
    Alterei os parametros da class Trade para receber apenas uma lista com os
    prices. Dessa forma não é necessário definir um objeto da classe Trade com
    buy, sell, etc. Adicionei também algumas alterações ao ficheiro readme.txt

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 class Trade:
-    def __init__(self, buy, sell, time, stop_loss=None, take_profit=None):
-        self.buy = buy
-        self.sell = sell
-        self.time = time
-        self.stop_loss = stop_loss if stop_loss is not None else None
-        self.take_profit = take_profit if take_profit is not None else None
+    def __init__(self, prices):
+        self.prices = prices
         
+    def buy(self):
+        return self.prices[0]
+
+    def sell(self):
+        return self.prices[-1]
+    
     def profit(self):
-        return self.sell - self.buy
+        return self.sell() - self.buy() 
 
     def profit_perc(self):
-        return (self.sell - self.buy)/self.buy
+        return self.profit()/self.buy()
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Este repositório contém os ficheiros em que desenvolvemos uma biblioteca
+em Python para fazer Backtesting de estratégias e algoritmos.
