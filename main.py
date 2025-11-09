cf8167a85467357fa46dbaeb5e638a27e2cc228e
Afonso Barão <afonso.j.m.barao@gmail.com>
2025-11-09 11:24:47 +0000

    Acabei a classe Trade e alterei a ToDoList.txt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-class 
+class Trade:
+    def __init__(self, buy, sell, time, stop_loss=None, take_profit=None):
+        self.buy = buy
+        self.sell = sell
+        self.time = time
+        self.stop_loss = stop_loss if stop_loss is not None else None
+        self.take_profit = take_profit if take_profit is not None else None
+        
+    def profit(self):
+        return self.sell - self.buy
+
+    def profit_perc(self):
+        return (self.sell - self.buy)/self.buy
